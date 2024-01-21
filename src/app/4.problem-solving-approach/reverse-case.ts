2ab2dc79ca6442a69a29bf5b14d99afde18761fd
aamanh30 <aamanh30@gmail.com>
2024-01-21 16:05:14 +0530

    refactor(problem-solving) - reverse-case - Updated the function to remove redundant curly brackets

--- src/app/4.problem-solving-approach/reverse-case.ts
+++ src/app/4.problem-solving-approach/reverse-case.ts
@@ -3,9 +3,9 @@ const upperCaseCodes = new Set(Array.from(new Array(26)).map((_, i) => i + 65));
 export const reverseCase = (str: string): string =>
   str
     .split('')
-    .map(char => {
-      return upperCaseCodes.has(char.charCodeAt(0))
+    .map(char =>
+      upperCaseCodes.has(char.charCodeAt(0))
         ? char.toLowerCase()
-        : char.toUpperCase();
-    })
+        : char.toUpperCase()
+    )
     .join('');
